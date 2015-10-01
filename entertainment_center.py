458e83840936e8a45ef06d6f610c38089e19fc12
nathanscamera <nathanscamera@gmail.com>
2015-09-30 21:59:33 -0400

    Movie site with modal bug.

---
+++ entertainment_center.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+import fresh_tomatoes
+import media
+
+daredevil_netflix = media.Movie(
+        "Daredevil",
+        "Daredevil follows Matt Murdock, attorney by day and vigilante by night. Blinded in an accident as a child, Murdock uses his heightened senses as Daredevil, fighting crime on the streets of New York after the sun goes down. His efforts are not welcomed by powerful businessman Wilson Fisk -- aka Kingpin -- and others whose interests collide with those of Daredevil. Though Murdock's day job portrays a man who believes in the criminal justice system, his alter ego suggests otherwise, as he takes the law into his own hands to protect his Hell's Kitchen neighborhood and the surrounding communities.",
+        "http://ia.media-imdb.com/images/M/MV5BMTgyMjU0Mzg5Nl5BMl5BanBnXkFtZTgwMTg3MDYyNTE@._V1_SX640_SY720_.jpg",
+        "https://www.youtube.com/watch?v=jAy6NJ_D5vU")
+
+daredevil = media.Movie(
+        "Daredevil",
+        "Attorney Matt Murdock (Ben Affleck) is blind, but his other four senses function with superhuman sharpness. By day, Murdock represents the downtrodden. At night, he is Daredevil, a masked vigilante, a relentless avenger of justice. When Wilson Fisk (Michael Clarke Duncan) hires Bullseye (Colin Farrell) to kill Daredevil, Murdock must rely on his own senses and search out the conspirators against justice -- which may include his own girlfriend, Elektra (Jennifer Garner).",
+        "https://upload.wikimedia.org/wikipedia/en/0/04/Daredevil_poster.JPG",
+        "https://www.youtube.com/watch?v=LmP3YFk_YHA")
+
+spider_man = media.Movie(
+        "Spider-man",
+        "Spider-Man centers on student Peter Parker (Tobey Maguire) who, after being bitten by a genetically-altered spider, gains superhuman strength and the spider-like ability to cling to any surface. He vows to use his abilities to fight crime, coming to understand the words of his beloved Uncle Ben: With great power comes great responsibility.",
+        "https://upload.wikimedia.org/wikipedia/en/f/f3/Spider-Man2002Poster.jpg",
+        "https://www.youtube.com/watch?v=O7zvehDxttM")
+
+spider_man2 = media.Movie(
+        "Spider-man 2",
+        "When a failed nuclear fusion experiment results in an explosion that kills his wife, Dr. Otto Octavius (Alfred Molina) is transformed into Dr. Octopus, a cyborg with deadly metal tentacles. Doc Ock blames Spider-Man (Tobey Maguire) for the accident and seeks revenge. Meanwhile, Spidey's alter ego, Peter Parker, faces fading powers and self-doubt. Complicating matters are his best friend's (James Franco) hatred for Spider-Man and his true love's (Kirsten Dunst) sudden engagement to another man.",
+        "https://upload.wikimedia.org/wikipedia/en/0/02/Spider-Man_2_Poster.jpg",
+        "https://www.youtube.com/watch?v=SCLTJJZ8gas")
+
+spider_man3 = media.Movie(
+        "Spider-man 3",
+        "Peter Parker (Tobey Maguire) and M.J. (Kirsten Dunst) seem to finally be on the right track in their complicated relationship, but trouble looms for the superhero and his lover. Peter's Spider-Man suit turns black and takes control of him, not only giving Peter enhanced power but also bringing out the dark side of his personality. Peter must overcome the suit's influence as two supervillains, Sandman and Venom, rise up to destroy him and all those he holds dear.",
+        "https://upload.wikimedia.org/wikipedia/en/7/7a/Spider-Man_3%2C_International_Poster.jpg",
+        "https://www.youtube.com/watch?v=MTIP-Ih_GR0")
+
+avengers = media.Movie(
+        "Avengers",
+        "When Thor's evil brother, Loki (Tom Hiddleston), gains access to the unlimited power of the energy cube called the Tesseract, Nick Fury (Samuel L. Jackson), director of S.H.I.E.L.D., initiates a superhero recruitment effort to defeat the unprecedented threat to Earth. Joining Fury's dream team are Iron Man (Robert Downey Jr.), Captain America (Chris Evans), the Hulk (Mark Ruffalo), Thor (Chris Hemsworth), the Black Widow (Scarlett Johansson) and Hawkeye (Jeremy Renner).",
+        "https://upload.wikimedia.org/wikipedia/en/f/f9/TheAvengers2012Poster.jpg",
+        "https://www.youtube.com/watch?v=eOrNdBpGMv8")
+
+avengers2 = media.Movie(
+        "Avengers: Age of Ultron",
+        "When Tony Stark (Robert Downey Jr.) jump-starts a dormant peacekeeping program, things go terribly awry, forcing him, Thor (Chris Hemsworth), the Incredible Hulk (Mark Ruffalo) and the rest of the Avengers to reassemble. As the fate of Earth hangs in the balance, the team is put to the ultimate test as they battle Ultron, a technological terror hell-bent on human extinction. Along the way, they encounter two mysterious and powerful newcomers, Pietro and Wanda Maximoff.",
+        "https://upload.wikimedia.org/wikipedia/en/1/1b/Avengers_Age_of_Ultron.jpg",
+        "https://www.youtube.com/watch?v=MZoO8QVMxkk")
+
+ironman = media.Movie(
+        "Ironman",
+        "A billionaire industrialist and genius inventor, Tony Stark (Robert Downey Jr.), is conducting weapons tests overseas, but terrorists kidnap him to force him to build a devastating weapon. Instead, he builds an armored suit and upends his captors. Returning to America, Stark refines the suit and uses it to combat crime and terrorism.",
+        "https://upload.wikimedia.org/wikipedia/en/7/70/Ironmanposter.JPG",
+        "https://www.youtube.com/watch?v=KAE5ymVLmZg")
+
+ironman2 = media.Movie(
+        "Ironman 2",
+        "With the world now aware that he is Iron Man, billionaire inventor Tony Stark (Robert Downey Jr.) faces pressure from all sides to share his technology with the military. He is reluctant to divulge the secrets of his armored suit, fearing the information will fall into the wrong hands. With Pepper Potts (Gwyneth Paltrow) and Rhodey Rhodes (Don Cheadle) by his side, Tony must forge new alliances and confront a powerful new enemy.",
+        "https://upload.wikimedia.org/wikipedia/en/e/ed/Iron_Man_2_poster.jpg",
+        "https://www.youtube.com/watch?v=FNQowwwwYa0")
+
+ironman3 = media.Movie(
+        "Ironman 3",
+        "Plagued with worry and insomnia since saving New York from destruction, Tony Stark (Robert Downey Jr.), now, is more dependent on the suits that give him his Iron Man persona -- so much so that every aspect of his life is affected, including his relationship with Pepper (Gwyneth Paltrow). After a malevolent enemy known as the Mandarin (Ben Kingsley) reduces his personal world to rubble, Tony must rely solely on instinct and ingenuity to avenge his losses and protect the people he loves.",
+        "https://upload.wikimedia.org/wikipedia/en/d/d5/Iron_Man_3_theatrical_poster.jpg",
+        "https://www.youtube.com/watch?v=2CzoSeClcw0")
+
+captain_america = media.Movie(
+        "Captain America",
+        "It is 1941 and the world is in the throes of war. Steve Rogers (Chris Evans) wants to do his part and join America's armed forces, but the military rejects him because of his small stature. Finally, Steve gets his chance when he is accepted into an experimental program that turns him into a supersoldier called Captain America. Joining forces with Bucky Barnes (Sebastian Stan) and Peggy Carter (Hayley Atwell), Captain America leads the fight against the Nazi-backed HYDRA organization.",
+        "https://upload.wikimedia.org/wikipedia/en/3/37/Captain_America_The_First_Avenger_poster.jpg",
+        "https://www.youtube.com/watch?v=-J3HfllvXWE")
+
+captain_america2 = media.Movie(
+        "The Winter Soldier",
+        "After the cataclysmic events in New York with his fellow Avengers, Steve Rogers, aka Captain America (Chris Evans), lives in the nation's capital as he tries to adjust to modern times. An attack on a S.H.I.E.L.D. colleague throws Rogers into a web of intrigue that places the whole world at risk. Joining forces with the Black Widow (Scarlett Johansson) and a new ally, the Falcon, Rogers struggles to expose an ever-widening conspiracy, but he and his team soon come up against an unexpected enemy.",
+        "https://upload.wikimedia.org/wikipedia/en/e/e8/Captain_America_The_Winter_Soldier.jpg",
+        "https://www.youtube.com/watch?v=7SlILk2WMTI")
+
+thor = media.Movie(
+        "Thor",
+        "As the son of Odin (Anthony Hopkins), king of the Norse gods, Thor (Chris Hemsworth) will soon inherit the throne of Asgard from his aging father. However, on the day that he is to be crowned, Thor reacts with brutality when the gods' enemies, the Frost Giants, enter the palace in violation of their treaty. As punishment, Odin banishes Thor to Earth. While Loki (Tom Hiddleston), Thor's brother, plots mischief in Asgard, Thor, now stripped of his powers, faces his greatest threat.",
+        "https://upload.wikimedia.org/wikipedia/en/f/fc/Thor_poster.jpg",
+        "https://www.youtube.com/watch?v=JOddp-nlNvQ")
+
+thor2 = media.Movie(
+        "Thor: The Dark World",
+        "In ancient times, the gods of Asgard fought and won a war against an evil race known as the Dark Elves. The survivors were neutralized, and their ultimate weapon -- the Aether -- was buried in a secret location. Hundreds of years later, Jane Foster (Natalie Portman) finds the Aether and becomes its host, forcing Thor (Chris Hemsworth) to bring her to Asgard before Dark Elf Malekith (Christopher Eccleston) captures her and uses the weapon to destroy the Nine Realms -- including Earth.",
+        "https://upload.wikimedia.org/wikipedia/en/7/7e/Thor_-_The_Dark_World_poster.jpg",
+        "https://www.youtube.com/watch?v=npvJ9FTgZbM")
+
+ant_man = media.Movie(
+        "Ant-Man",
+        "Forced out of his own company by former protégé Darren Cross, Dr. Hank Pym (Michael Douglas) recruits the talents of Scott Lang (Paul Rudd), a master thief just released from prison. Lang becomes Ant-Man, trained by Pym and armed with a suit that allows him to shrink in size, possess superhuman strength and control an army of ants. The miniature hero must use his new skills to prevent Cross, also known as Yellowjacket, from perfecting the same technology and using it as a weapon for evil.",
+        "https://upload.wikimedia.org/wikipedia/en/7/75/Ant-Man_poster.jpg",
+        "https://www.youtube.com/watch?v=pWdKf3MneyI")
+
+elektra = media.Movie(
+        "Elektra",
+        "Assassin-for-hire Elektra (Jennifer Garner) works for a mysterious international organization known as the Hand, for which she kills her targets without question, and in a conspicuous red bustier, no less. When the Hand asks her to kill Mark Miller (Goran Visnjic) and his daughter, Abby (Kirsten Prout), while they're on holiday, Elektra's conscience kicks in and she decides to protect her targets rather than eliminate them. Of course, the Hand isn't so willing to let them off the hook.",
+        "https://upload.wikimedia.org/wikipedia/en/2/21/Elektra_teaser.jpg",
+        "https://www.youtube.com/watch?v=O0HDDJKrxY8")
+
+ghost_rider = media.Movie(
+        "Ghost Rider",
+        "Years ago, motorcycle stuntman Johnny Blaze (Nicolas Cage) sold his soul to save the life of a loved one. Now, he transforms into a fiery, avenging agent of justice at night wherever evil roams. As the only one who can walk in both worlds, Johnny is charged by Mephistopheles (Donal Logue) himself to destroy Blackheart (Wes Bentley), a demon that wants to overthrow Mephistopheles and create a worse hell than the one that currently exists.",
+        "https://upload.wikimedia.org/wikipedia/en/7/71/GhostRiderBigPoster.jpg",
+        "https://www.youtube.com/watch?v=KSmYi4lhfrM")
+
+catwoman = media.Movie(
+        "Catwoman",
+        "Catwoman is the story of shy, sensitive artist Patience Philips (Halle Berry), a woman who can't seem to stop apologizing for her own existence. She works as a graphic designer for Hedare Beauty, a mammoth cosmetics company on the verge of releasing a revolutionary anti-aging product. When Patience inadvertently happens upon a dark secret her employer is hiding, she finds herself in the middle of a corporate conspiracy. What happens next changes Patience forever.",
+        "https://upload.wikimedia.org/wikipedia/en/0/04/Catwoman_poster.jpg",
+        "https://www.youtube.com/watch?v=d6pJbjbRnAA")
+
+x_men = media.Movie(
+        "X-Men",
+        "They are children of the atom, homo superior, the next link in the chain of evolution. Each was born with a unique genetic mutation, which at puberty manifested itself in extraordinary powers. In a world filled with hate and prejudice, they are feared by those who cannot accept their differences. Led by Xavier the X-Men fight to protect a world that fears them. They are locked in a battle with former colleague and friend, Magneto who believes humans and mutants should never co-exist.",
+        "https://upload.wikimedia.org/wikipedia/en/8/8c/XMen1poster.jpg",
+        "https://www.youtube.com/watch?v=Iy5R5_T243w")
+
+x2 = media.Movie(
+        "X2",
+        "Stryker (Brian Cox), a villianous former Army commander, holds the key to Wolverine's (Hugh Jackman) past and the future of the X-Men. This threat re-ignites the call for a mutant registration act. Stryker starts a full-out assault on Professor Xavier's (Patrick Stewart) mansion and school. After escaping his plastic cell, Magneto (Ian McKellen) proposes a partnership with Xavier and the X-Men to combat this new formidable enemy they both have in common.",
+        "https://upload.wikimedia.org/wikipedia/en/3/3e/X2_poster.jpg",
+        "https://www.youtube.com/watch?v=voxI4h7Jzmo")
+
+x_men_last_stand = media.Movie(
+        "X-Men: The Last Stand",
+        "The discovery of a cure for mutations leads to a turning point for Mutants (Hugh Jackman, Halle Berry, Famke Janssen, Anna Paquin, Rebecca Romijn, Kelsey Grammer). They may now choose to give up their powers and become fully human or retain their uniqueness and remain isolated. War looms between the followers of Charles Xavier (Patrick Stewart), who preaches tolerance, and those of Magneto (Ian McKellen), who advocates survival of the fittest.",
+        "https://upload.wikimedia.org/wikipedia/en/5/56/X-Men_The_Last_Stand.jpg",
+        "https://www.youtube.com/watch?v=dv-7FY5gm0w")
+
+deadpool = media.Movie(
+        "Deadpool",
+        "Armed with accelerated healing powers and a twisted sense of humor, former mercenary Wade Wilson (Ryan Reynolds) goes in search of the man who nearly destroyed his life.",
+        "https://upload.wikimedia.org/wikipedia/en/4/46/Deadpool_poster.jpg",
+        "https://www.youtube.com/watch?v=FyKWUTwSYAs")
+
+movies = [deadpool, daredevil_netflix, daredevil, spider_man, spider_man2, spider_man3, avengers, avengers2, ironman, ironman2, ironman3, captain_america, captain_america2, thor, thor2, ant_man, ghost_rider, x_men, x2, x_men_last_stand]
+fresh_tomatoes.open_movies_page(movies)
